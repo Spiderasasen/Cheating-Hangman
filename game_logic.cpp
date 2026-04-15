e3b89c7314f4891cd6d54c015919e52784ef5921
spiderasasen <spiderasasen@gmail.com>
2026-04-15 15:36:20 -0400

    commenting the section so i can better understand

--- game_logic.cpp
+++ game_logic.cpp
@@ -11,12 +11,14 @@
 
 using namespace std;
 
+//constructor
 Game::Game(const vector<string>& dict) {
     dictionary = dict;
     hard_mode = false;
     lives = 0;
 }
 
+//game diffculty for the class
 void Game::chooseDifficulty() {
     char choice;
     cout << "Play in hard mode? (y/n): ";
@@ -26,31 +28,39 @@ void Game::chooseDifficulty() {
     hard_mode = (choice == 'y');
 }
 
+//sets the word length of the class
 void Game::initializeWordLength(int length) {
     possible_words = filter_size(dictionary, length);
 
     pattern = string(length, '-');
 
-    if (hard_mode)
+    //little mini mechanic
+    if (hard_mode) //if hard mode is avilible, then whatever the length of the word, is the number of lives the user has
         lives = length;
     else
         lives = 5;
 }
 
+//prints the status of the game
 void Game::printStatus() const {
+    //current pattern of the game
     cout << "\nPattern: " << pattern << endl;
 
+    //what guesses the user entered
     cout << "Guesses: ";
     for (char c : guesses) cout << c << " ";
     cout << endl;
 
+    //what letters where misses
     cout << "Misses: ";
     for (char c : misses) cout << c << " ";
     cout << endl;
 
+    //number of lives that exist
     cout << "Lives: " << lives << "\n" << endl;
 }
 
+//gets the number of guesses from the user
 char Game::getPlayerGuess() {
     char g;
     cout << "Enter a letter: ";
@@ -58,13 +68,16 @@ char Game::getPlayerGuess() {
     return tolower(g);
 }
 
+//checks if the user has already guessed the letter
 bool Game::alreadyGuessed(char c) const {
     return find(guesses.begin(), guesses.end(), c) != guesses.end();
 }
 
+//handels the players guess
 bool Game::processGuess(char guess) {
     guesses.push_back(guess);
 
+    //for cheating
     vector<string> new_list = filtering_via_word(possible_words, guess);
 
     // Build pattern from any word in the new list
@@ -78,28 +91,33 @@ bool Game::processGuess(char guess) {
         }
     }
 
+    //checks if the guess revails something, and if it doesn't, it removes a life
     bool revealed = (merged != pattern);
-
     if (!revealed) {
         lives--;
         misses.push_back(guess);
     }
 
+    //updates the pattern and also updates the possible list of words
     pattern = merged;
     possible_words = new_list;
 
     return revealed;
 }
 
+//checking if the user won the game, by looking if there are no '-'
 bool Game::isWin() const {
     return pattern.find('-') == string::npos;
 }
 
+//checks if the user lost
 bool Game::isLose() const {
     return lives <= 0;
 }
 
+//main game system that plays the game
 void Game::play() {
+    //loops until the user wins or not
     while (!isWin() && !isLose()) {
         printStatus();
 
@@ -113,9 +131,12 @@ void Game::play() {
         processGuess(guess);
     }
 
+    //checks if the user wins
     if (isWin()) {
         cout << "You win! The word was: " << pattern << endl;
-    } else {
+    }
+    //else the user lost
+    else {
         string final_word = possible_words[rand() % possible_words.size()];
         cout << "You lose! The word was: " << final_word << endl;
     }
